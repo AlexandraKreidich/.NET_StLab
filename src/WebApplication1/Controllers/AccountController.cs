ec066c58cf2431e43e606c83345cfe43278e9a00
Alexandra Kreidich <kreidichalexandra@gmail.com>
2018-02-12 19:48:03 +0300

    some new fales created

---
+++ src/WebApplication1/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class AccountController : Controller
+    {
+        // GET api/account/login
+        [HttpGet]
+        public string Login()
+        {
+            return "login";
+        }
+    }
+}
