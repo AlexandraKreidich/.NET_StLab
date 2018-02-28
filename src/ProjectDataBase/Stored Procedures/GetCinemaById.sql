dd09cb5da1b6c49526f8d0d97bcc218f5f737c41
Alexandra Kreidich <kreidichalexandra@gmail.com>
2018-02-28 10:54:45 +0300

    the problem with dependensies fixed

---
+++ src/ProjectDataBase/Stored Procedures/GetCinemaById.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[GetCinemaById]
+    @Id int
+AS
+    SELECT
+        Cinema.Name,
+        Cinema.City,
+            (SELECT COUNT(CinemaId)
+            FROM Hall
+            WHERE CinemaId = @Id) AS
+        HallsNumber
+    FROM Cinema
+    WHERE Cinema.Id = @Id
