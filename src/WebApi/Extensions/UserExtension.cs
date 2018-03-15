580a7a40a2692eba3b7a198a19f0cc87ca0c956c
Alexandra Kreidich <kreidichalexandra@gmail.com>
2018-03-15 13:16:50 +0300

    User Extension Created

---
+++ src/WebApi/Extensions/UserExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+
+namespace WebApi.Extensions
+{
+    public static class UserExtension
+    {
+        public static int GetUserId(this ClaimsPrincipal user)
+        {
+            return int.Parse(user.FindFirst(ClaimTypes.NameIdentifier).Value);
+        }
+    }
+}
