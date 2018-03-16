61f445d5c9edf9a2ef8ac33f60fdc38e768f2803
Alexandra Kreidich <kreidichalexandra@gmail.com>
2018-03-16 10:09:49 +0300

    Service provider deleted from method

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -75,7 +75,7 @@ namespace WebApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
